0274a55f04cfb959e16575dbbb8fb1af2cc54edd
DaniellaErven <daniella.erven@protonmail.com>
2022-04-24 21:43:48 -0600

    changes some file paths

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,19 @@
 package main
 
 //CheckIp "github.com/DaniMiaErv/AvoxiProject"
+import (
+	"net/http"
+	"os"
+
+	//checkip "github.com/DaniMiaErv/AvoxiProject/pkg/ipcheck"
+	checkip "github.com/DaniMiaErv/AvoxiProject"
+
+	"github.com/go-kit/kit/endpoint"
+	kitlog "github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	httptransport "github.com/go-kit/kit/transport/http"
+	"github.com/gorilla/mux"
+)
 
 func main() {
 
@@ -9,4 +22,24 @@ func main() {
 
 	//println(CheckIp.CheckIPWhiteList(testIp, countryList))
 
+	var logger kitlog.Logger
+	{
+		logger = kitlog.NewLogfmtLogger(os.Stderr)
+		logger = kitlog.With(logger, "timestamp", kitlog.DefaultTimestampUTC)
+		logger = kitlog.With(logger, "caller", kitlog.DefaultCaller)
+	}
+
+	var middlewares []endpoint.Middleware
+	var options []httptransport.ServerOption
+	svc := checkip.NewService(logger)
+	eps := checkip.MakeEndpoints(svc, logger, middlewares)
+	r := mux.NewRouter()
+	r.Methods(http.MethodGet).Path("/palindrome").Handler(checkip.GetIsPalHandler(eps.GetIsPalindrome, options))
+	r.Methods(http.MethodGet).Path("/reverse").Handler(checkip.GetReverseHandler(eps.GetReverse, options))
+	level.Info(logger).Log("status", "listening", "port", "8080")
+	svr := http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: r,
+	}
+	level.Error(logger).Log(svr.ListenAndServe())
 }
